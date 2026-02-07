024d5a731563b0db02146cafc13dc7fa863f4966
ShraddhaMistry07 <shraddhamistry2007@gmail.com>
2026-02-07 12:47:46 +0530

    Add files via upload

---
+++ practical1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+   printf("\n Arithmetic Operations");
+   int a;
+   int b;
+
+   printf("\n Enter the value of a and b:");
+   scanf("\n %d,%d",&a,&b);
+   printf("\n Addition is:%d",a+b);
+   printf("\n Subtraction is:%d",a-b);
+   printf("\n Multiplication is:%d",a*b);
+   printf("\n Division is:%d",a/b);
+   printf("\n Reminder is:%d",a%b);
+
+   return 0; }
+
